f6d32a217ff3f68587be47b285369ab287a474ba
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-07 19:41:21 +0700

    Added check function for equality

--- src/codility/lesson_01.kt
+++ src/codility/lesson_01.kt
@@ -11,6 +11,6 @@ fun main() {
 
 fun solutionOfBinaryGap(N: Int): Int {
     val str = Integer.toBinaryString(N)
-    val arr = str.split("1").map { it.length};
+    val arr = str.split("1").map { it.length}
     return if (str.last() == '0') arr.dropLast(1).max()!! else arr.max()!!
 }
