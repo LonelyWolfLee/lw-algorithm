8ffd4327e7c22e84bb2f8a1db46cfc20a8a79e25
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-07 19:13:27 +0700

    Use 'it' for subroutine

--- src/codility/lesson_01.kt
+++ src/codility/lesson_01.kt
@@ -9,6 +9,6 @@ fun main() {
 
 fun solutionOfBinaryGap(N: Int): Int {
     val str = Integer.toBinaryString(N)
-    val arr = str.split("1").map { seq -> seq.length};
+    val arr = str.split("1").map { it.length};
     return if (str.last() == '0') arr.dropLast(1).max()!! else arr.max()!!
 }
