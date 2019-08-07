f2546f41e901f0ee5a9f8fa14d15d672cdc63e20
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-07 18:50:19 +0700

    Removed empty line

--- src/codility/lesson_01.kt
+++ src/codility/lesson_01.kt
@@ -8,7 +8,6 @@ fun main() {
 }
 
 fun solutionOfBinaryGap(N: Int): Int {
-
     val str = Integer.toBinaryString(N)
     val arr = str.split("1").map { seq -> seq.length};
     return if (str.last() == '0') arr.dropLast(1).max()!!
