7c3bcf0058936c60dd4f0b35b5a449d1ade84a49
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-09 12:53:19 +0700

    Added solution of lesson 03

--- src/codility/README.md
+++ src/codility/README.md
@@ -7,6 +7,7 @@
 DIFFICULTY : PAINLESS  
 GOAL : Find longest sequence of zeros in binary representation of an integer.
 
+
 ### [Lesson 02](https://app.codility.com/programmers/lessons/2-arrays/) : Arrays
 
 ###### 1. [OddOccurrencesInArray](https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/)
@@ -18,3 +19,22 @@ GOAL : Find value that occurs in odd number of elements.
 
 DIFFICULTY : PAINLESS  
 GOAL : Rotate an array to the right by a given number of steps.
+
+
+### [Lesson 03](https://app.codility.com/programmers/lessons/3-time_complexity/) : Time Complexity
+
+###### 1. [FrogJmp](https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/)
+
+DIFFICULTY : PAINLESS  
+GOAL : Count minimal number of jumps from position X to Y.
+
+###### 2. [PermMissingElem](https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/)
+
+DIFFICULTY : PAINLESS  
+GOAL : Find the missing element in a given permutation.
+
+###### 3. [TapeEquilibrium]
+
+DIFFICULTY : PAINLESS  
+GOAL : Minimize the value |(A[0] + ... + A[P-1]) - (A[P] + ... + A[N-1])|.
+
---
+++ src/codility/lesson_03.kt
@@ -0,0 +1,48 @@
+package codility
+
+import utils.checkValue
+import kotlin.math.abs
+
+fun main() {
+    println("Lesson 03 : Time Complexity")
+
+    // 1. FrogJmp
+    checkValue(solutionOfFrogJmp(10, 85, 30), 3)
+
+    // 2. PermMissingElem
+    checkValue(solutionOfPermMissingElem(arrayOf(2, 3, 1, 5).toIntArray()), 4)
+
+    // 3. TapeEquilibrium
+    checkValue(solutionOfTapeEquilibrium(arrayOf(3, 1, 2, 4, 3).toIntArray()), 1)
+    checkValue(solutionOfTapeEquilibrium(arrayOf(1000, -1000).toIntArray()), 2000)
+}
+
+// 100% O(1)
+fun solutionOfFrogJmp(X: Int, Y: Int, D: Int): Int {
+    if (X == Y) return 0
+    val N = (Y-X) / D
+    val C = (Y-X) % D
+    return if (C == 0) N else N+1
+}
+
+// 100% O(N) or O(NlogN)
+fun solutionOfPermMissingElem(A: IntArray): Int {
+    val actual = A.sum().toBigInteger()
+    val expect = ((A.size+1).toBigInteger() * (A.size+2).toBigInteger()).divide(2.toBigInteger())
+    return (expect - actual).toInt()
+}
+
+// 100% O(N)
+fun solutionOfTapeEquilibrium(A: IntArray): Int {
+    var sum = A.sum()
+    var acc = 0
+    var minDiff = 2000
+    for (i in 0 until A.size-1) {
+        acc += A[i]
+        sum -= A[i]
+        val diff = abs(sum-acc)
+        if (diff < minDiff) minDiff = diff
+    }
+
+    return minDiff
+}
